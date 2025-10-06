aa0e819fdc0577a8ff59a9a8f409cf55dfc00fa4
GabrielGPena <kbeca.gabriel@hotmail.com>
2025-10-06 19:57:33 -0300

    feat: enhance HomeHeader to display total and add Separator component

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,14 +1,27 @@
 import { Text, View } from "react-native";
-import { style } from "./styles";
 import { LinearGradient } from "expo-linear-gradient"
-import { colors } from "@/theme";
 
-export function HomeHeader() {
+import { style } from "./styles";
+import { colors } from "@/theme";
+import { Separator } from "../Separator";
+
+export type HomeHeaderProps = {
+  total: string
+}
+
+type Props = {
+  data: HomeHeaderProps
+}
+
+export function HomeHeader({ data }: Props) {
   return (
     <LinearGradient colors={[colors.blue[500], colors.blue[800]]} style={style.container}>
       <View>
         <Text style={style.label}>Total que você possui</Text>
+        <Text style={style.total}>{data.total}</Text>
       </View>
+
+      <Separator color={colors.blue[400]} />
     </LinearGradient>
   )
 }
