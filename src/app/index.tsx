ed53e63fdb8254d4ea1a9f10c508bfefe8276ad1
GabrielGPena <kbeca.gabriel@hotmail.com>
2025-10-06 22:38:25 -0300

    feat: add Target component with styles and integrate it into Index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
 
 const summary = {
   total: "R$ 2.680,00",
@@ -8,10 +9,21 @@ const summary = {
   output: "R$ 150,00"
 }
 
+const targets = [
+  {
+    current: "R$ 500,00",
+    name: "Comprar cadeira gamer",
+    percentage: "50%",
+    target: "R$ 1.000,00"
+  }
+]
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <Target data={targets[0]} />
     </View>
   )
 }
