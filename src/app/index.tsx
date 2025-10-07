d756f7a4d37e1a5e6a91126219b1273f6e316c08
GabrielGPena <kbeca.gabriel@hotmail.com>
2025-10-07 13:15:25 -0300

    feat: add Button component for user interactions and integrate it into Index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
 import { View } from "react-native";
+import { router } from "expo-router"
 
 import { HomeHeader } from "@/components/HomeHeader";
 import { Target } from "@/components/Target";
 import { List } from "@/components/List";
+import { Button } from "@/components/Button";
 
 const summary = {
   total: "R$ 2.680,00",
@@ -43,10 +45,19 @@ export default function Index() {
         title="Metas"
         data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta registrada ainda."
         containerStyle={{ paddingHorizontal: 24 }}
       />
+
+      <View style={{ padding: 24, paddingBottom: 62 }}>
+        <Button title="Adicionar nova meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   )
 }
