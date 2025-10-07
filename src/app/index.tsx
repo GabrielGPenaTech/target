ae789c33c0efa1210505c6eb2cae50cd1c963222
GabrielGPena <kbeca.gabriel@hotmail.com>
2025-10-06 21:46:23 -0300

    feat: update HomeHeader to use summary data for input and output values

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,16 @@ import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
 
+const summary = {
+  total: "R$ 2.680,00",
+  input: "R$ 1.500,00",
+  output: "R$ 150,00"
+}
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
-      <HomeHeader data={{ total: "R$ 2.680,00" }} />
+      <HomeHeader data={summary} />
     </View>
   )
 }
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -5,10 +5,12 @@ import { style } from "./styles";
 import { colors } from "@/theme";
 
 import { Separator } from "../Separator";
-import { Summary } from "../Summary";
+import { Summary, SummaryProps } from "../Summary";
 
 export type HomeHeaderProps = {
   total: string
+  input: string
+  output: string
 }
 
 type Props = {
@@ -27,12 +29,12 @@ export function HomeHeader({ data }: Props) {
 
       <View style={style.summaryContainer}>
         <Summary
-          data={{ label: "Entradas", value: "R$ 6.184,90" }}
+          data={{ label: "Entradas", value: data.input }}
           icon={{ name: "arrow-upward", color: colors.green[500] }}
         />
 
         <Summary
-          data={{ label: "Saídas", value: "-R$ 883,65" }}
+          data={{ label: "Saídas", value: data.output }}
           icon={{ name: "arrow-downward", color: colors.red[400] }}
           isLabelPositionLeft
         />
