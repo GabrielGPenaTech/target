28cef72a03a28a2005cfebbab333a5bb99b15690
GabrielGPena <kbeca.gabriel@hotmail.com>
2025-10-07 22:52:28 -0300

    feat: add Progress component for displaying savings progress and integrate it into InProgress

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,13 +1,28 @@
-import { router, useLocalSearchParams } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+import { useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+
+const details = {
+  current: "580,00",
+  target: "R$ 1.790,00",
+  percentage: 50
+}
 
 export default function InProgress() {
   const param = useLocalSearchParams<{ id: string }>()
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>In-progress: {param.id}</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => { }
+        }}
+      />
+
+      <Progress data={details} />
     </View>
   )
 }
